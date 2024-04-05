f1534d839df29b10bcdcb67937cbdaa008b75cf2
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-04-05 10:35:58 +0200

    remove local

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,16 +2,7 @@
 export default async function authMiddleware(req, res, next) {
   let userData;
 
-  if (process.env.NODE_ENV_PROFILE === 'local') {
-    userData = {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
-      password: 'SecurePassword123!',
-      role: 'restaurant',
-    };
-  } else if (req.headers['x-user']) {
+  if (req.headers['x-user']) {
     userData = JSON.parse(req.headers['x-user']);
   }
 
